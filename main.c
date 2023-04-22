2038c87dc5dd2bd3c160fb57d5f1903305c4a01f
Rogereduaardo <remmtools98@gmail.com>
2023-04-22 14:29:05 +0000

    Main based code

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
+// Stepwise refinement #1
 #include <stdio.h>
+#include "payroll.h"
+#include "employee.h"
+#include "fileio.h"
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+#define PAYROLL_FILE "payroll.txt"
+
+int main() {
+    // Display program title and developer name
+   // ...
+
+    // Initialize system
+    payroll_init();
+    fileio_init(PAYROLL_FILE);
+    
+    // Read all the payroll entries
+    // ...
+
+   // Display results
 }
