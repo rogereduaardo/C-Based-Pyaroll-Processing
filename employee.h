8c59f5d05af4eaa5020ba4ea8929d2f8bb4db668
Rogereduaardo <remmtools98@gmail.com>
2023-04-22 14:23:03 +0000

    New Files added

---
+++ employee.h
@@ -0,0 +1,13 @@
+#ifndef __EMPLOYEE_H__
+#define __EMPLOYEE_H__
+
+typedef struct {
+    char firstName[32];
+    char lastName[32];
+    char gender;  //M for male, F for female, T for Third Gender
+    int tenure;
+    char rate; //H for hourly, W for weekly
+    double salary;
+} employee_t;
+
+#enddif
