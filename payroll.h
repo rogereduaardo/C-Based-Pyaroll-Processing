882b86ab830b8ca219f9cfeff5c6f6ba63b1b942
Rogereduaardo <remmtools98@gmail.com>
2023-04-22 14:35:51 +0000

    Based code fro employee files

---
+++ payroll.h
@@ -0,0 +1,19 @@
+#ifndef __PAYROLL_H_
+#define __PAYROLL_H_
+
+#include "emplyee.h"
+
+/ Return 0 on success or -1 on error
+int payroll_init();
+
+// Return 0 on success or -1 on error 
+int payroll_add(employee_t *employee);
+
+// Return number of employees
+int payroll_get_num_employees()
+
+// Return number of employees based on gender
+// Input param c is M, F, or T
+int payroll_get_num_employees_by_gender(char c);
+
+#endif
